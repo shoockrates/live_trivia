c3a6d9664de794fa54bffbdd053aa0afc1818699
Karolis Paskevicius <kp704595@gmail.com>
2025-10-13 20:43:37 +0300

    Updated scoring logic

--- LiveTriviaBackend/Models/Game.cs
+++ LiveTriviaBackend/Models/Game.cs
@@ -94,11 +94,11 @@ public class Game : BaseEntity
 
             if (playerAnswer != null)
             {
-                bool isCorrect = question.CorrectAnswerIndexes.All(i => playerAnswer.SelectedAnswerIndexes.Contains(i)) &&
+                playerAnswer.isCorrect= question.CorrectAnswerIndexes.All(i => playerAnswer.SelectedAnswerIndexes.Contains(i)) &&
                                  playerAnswer.SelectedAnswerIndexes.All(i => question.CorrectAnswerIndexes.Contains(i));
-                if (isCorrect)
+                if (playerAnswer.isCorrect)
                 {
-                    switch (question.Difficulty)
+                    switch (question.Difficulty.ToLower())
                     {
                         case "easy":
                             player.Score += 1;
