6934892094e8d6a5acfd8430b03b8987be00a7dc
Eigintas Urbanavičius <eigintas.urbanavicius@mif.stud.vu.lt>
2025-10-19 22:24:58 +0300

    Made changes to questions.json and redid migrations

--- LiveTriviaBackend/Migrations/20251019185803_SyncWithRepo.Designer.cs
+++ LiveTriviaBackend/Migrations/20251019185803_SyncWithRepo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using live_trivia.Data;
@@ -11,9 +12,11 @@ using live_trivia.Data;
 namespace LiveTriviaBackend.Migrations
 {
     [DbContext(typeof(TriviaDbContext))]
-    partial class TriviaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251019185803_SyncWithRepo")]
+    partial class SyncWithRepo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
