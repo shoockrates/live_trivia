da4767ba07ec62089972e29b3a3e7b36d0379cb1
Eigintas Urbanavičius <eigintas.urbanavicius@mif.stud.vu.lt>
2025-10-21 14:50:19 +0300

    Moved Interfaces folder to root

--- LiveTriviaBackend/Services/Interfaces/IAuthService.cs
+++
@@ -1,8 +0,0 @@
-
-namespace live_trivia.Services;
-
-public interface IAuthService
-{
-    Task<AuthResponse> LoginAsync(LoginRequest loginRequest);
-    Task<AuthResponse> RegisterAsync(RegisterRequest registerRequest);
-}
